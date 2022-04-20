237785dc543c6bd657e3d952b89b8dc3dc42ff87
P J Sreedeep <95912047+sreedeep1999@users.noreply.github.com>
2022-04-20 14:58:47 +0530

    Add files via upload

---
+++ armstrong.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+echo " Enter a number "
+read num
+dup=$num
+s=0
+while [ $num -gt 0 ]
+do
+r=$(( $num % 10 ))
+s=$((s+$((r*r*r)) ))
+num=$((num / 10))
+done
+if [ $dup -eq $s ]
+then
+echo "$dup is armstrong number"
+else
+echo "$dup is not armstrong number"
+fi
+ 
+
